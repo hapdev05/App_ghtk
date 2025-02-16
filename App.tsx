d9a72b81507b9b6bb9e607eab7143a9e0c6f1c55
ANH PHI <hapdev@Mac-mini-cua-ANH.local>
2025-02-16 13:23:31 +0700

    1234

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { styled } from 'nativewind';
+
+const StyledView = styled(View)
+const StyledText = styled(Text)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <StyledView className="flex-1 items-center justify-center bg-white">
+      <StyledText className="text-xl font-bold text-red-600">NativeWind is now installed!</StyledText>
       <StatusBar style="auto" />
-    </View>
+    </StyledView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
